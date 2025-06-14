9f7a812bcec0d0856ca7283e8a42cf5ddc479e96
VKSawna <varindersawna01@gmail.com>
2025-06-14 21:27:10 +0530

    Initialize First React Project with Vite, React, and ESLint
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added App component to render a welcome message.
    - Created Firstcompo component with a button and a heading.
    - Set up main entry point to render App and Firstcompo components.
    - Configured Vite with React plugin for development.

---
+++ FirstReactProject/src/Firstcompo.jsx
@@ -0,0 +1,11 @@
+function Firstcompo()
+{
+  return(
+    <>
+    <button>Hello</button>
+    <h2>This is button</h2>
+    </>
+
+  )
+}
+export default Firstcompo;
---
+++ FirstReactProject/src/main.jsx
@@ -0,0 +1,12 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Firstcompo from './Firstcompo.jsx'
+
+createRoot(document.getElementById('root')).render(
+
+    <>
+    <App />
+    <Firstcompo />
+    </>
+)
